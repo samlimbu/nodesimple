e0511b6371f33e574e412bac070e43ca028365ba
samlimbu <samlimbu@hotmail.com>
2018-05-05 20:38:07 +0545

    add

--- config/database.js
+++ config/database.js
@@ -5,4 +5,4 @@ module.exports = {
 }
 
 //kannel
-//http://127.0.0.1:13013/cgi-bin/sendsms?username=unifun&password=unifun&from=Unifun&to=9841759703&text=Hello&dlr-mask=31&drl-url=http
+//http://127.0.0.1:13013/cgi-bin/sendsms?username=unifun&password=unifun&from=Unifun&to=9841759703&text=Hello&dlr-mask=31&drl-url=http://nodeexpresstest.herokuapp.com/getParam?CampaignID={CampaignID}&MSISDN={MobileNumber}&Response=%d
